5575fb81ec2a715ea62fabd48e5ec09c796340f0
William Hegedusich <whegedusich@gmail.com>
2022-09-07 23:41:47 -0500

    updated main function to create and filter NeatTweet instances

--- twinter/__main__.py
+++ twinter/__main__.py
@@ -7,6 +7,7 @@ from sys import argv
 
 from twinter.client import TwitterClient
 from twinter.features import make_features
+from twinter.common.klasses import NeatTweet
 
 
 def twinter_main(username: str):
@@ -18,7 +19,9 @@ def twinter_main(username: str):
     with TwitterClient() as t:
         tweets = t.get_tweets(username)
 
-    make_features(tweets)
+    neat_tweets = list(map(NeatTweet, tweets))
+    neat_tweets = [t for t in neat_tweets if t.not_empty]
+    make_features(neat_tweets)
 
 
 if __name__ == '__main__':
