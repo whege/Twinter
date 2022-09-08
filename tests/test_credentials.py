ee9d4f6af591be721e6665aada2a66a5960b3f0a
William Hegedusich <whegedusich@gmail.com>
2022-09-08 00:50:56 -0500

    updated file to be a unittest file

--- tests/test_credentials.py
+++ tests/test_credentials.py
@@ -6,20 +6,30 @@ __doc__ = """Enter some text here, bitch"""
 
 from os import environ
 
+import unittest
+
 import tweepy
 from dotenv import load_dotenv
 
-load_dotenv()
 
-auth = tweepy.OAuth1UserHandler(
-    access_token=environ['ACCESS_TOKEN'],
-    access_token_secret=environ['ACCESS_TOKEN_SECRET'],
-    consumer_key=environ['API_KEY'],
-    consumer_secret=environ['API_KEY_SECRET']
-)
+class MyTestCase(unittest.TestCase):
+    @staticmethod
+    def test_something():
+        load_dotenv()
 
-api = tweepy.API(auth)
+        auth = tweepy.OAuth1UserHandler(
+            access_token=environ['ACCESS_TOKEN'],
+            access_token_secret=environ['ACCESS_TOKEN_SECRET'],
+            consumer_key=environ['API_KEY'],
+            consumer_secret=environ['API_KEY_SECRET']
+        )
 
-public_tweets = api.home_timeline()
-for tweet in public_tweets:
-    print(tweet.text)
+        api = tweepy.API(auth)
+
+        public_tweets = api.home_timeline()
+        for tweet in public_tweets:
+            print(tweet.text)
+
+
+if __name__ == '__main__':
+    unittest.main()
